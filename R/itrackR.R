7710849a5eb96e9154742685bdfb52737c854ff6
jashubbard <jashubbard@gmail.com>
2015-11-11 14:20:31 -0800

    First working version

--- R/itrackR.R
+++ R/itrackR.R
@@ -1,18 +1,18 @@
 
 
 #class constructor
-itrackr <- function(edfs = NULL,path=NULL,pattern=NULL,idvar='ID',resolution=c(1024,768),samples=FALSE)
+itrackr <- function(edfs = NULL,path=NULL,pattern=NULL,resolution=c(1024,768))
 {
 
   me <- list(
               edfs = edfs,
               ids = NULL,
-              idvar = idvar,
+              idvar = 'ID',
               indexvars = NULL,
               timevars = NULL,
               resolution = resolution,
               header = data.frame,
-              samples = data.frame,
+              samples = list(),
               fixations = data.frame,
               saccades = data.frame,
               blinks = data.frame,
@@ -31,7 +31,7 @@ itrackr <- function(edfs = NULL,path=NULL,pattern=NULL,idvar='ID',resolution=c(1
     }
 
   if(!is.null(path))
-    me <- load_edfs(me,path=path,pattern=pattern,samples=samples)
+    me <- load_edfs(me,path=path,pattern=pattern)
 
 
   return(me)
@@ -39,18 +39,16 @@ itrackr <- function(edfs = NULL,path=NULL,pattern=NULL,idvar='ID',resolution=c(1
 }
 
 
-load_edfs <- function(obj,path='.',pattern='*.edf',samples=FALSE,recursive = FALSE){
+load_edfs <- function(obj,path='.',pattern='*.edf',recursive = FALSE){
   UseMethod('load_edfs',obj)
 }
 
-load_edfs.itrackR <- function(obj,path='.',pattern='*.edf',samples=FALSE,recursive = FALSE){
+load_edfs.itrackR <- function(obj,path='.',pattern='*.edf',recursive = FALSE){
 
   fields <- c('fixations','saccades','blinks','messages','header')
 
-  if(samples)
-    fields <- c(fields,'samples')
 
-  allbatch <- edfR::edf.batch(path,pattern=pattern,samples=samples,do.plot=F)
+  allbatch <- edfR::edf.batch(path,pattern=pattern,samples=F,do.plot=F)
 
   alldata <- edfR::combine.eyedata(allbatch,fields=fields)
 
@@ -60,8 +58,11 @@ load_edfs.itrackR <- function(obj,path='.',pattern='*.edf',samples=FALSE,recursi
   obj$blinks <-alldata$blinks
   obj$messages <- alldata$messages
 
-  if(samples)
-    obj$samples <- alldata$samples
+#   if(samples)
+#     {
+#       allsamples <- load_samples(obj)
+#       obj$samples <- allsamples$samples
+#     }
 
   if(is.null(obj$edfs))
     obj$edfs <- unlist(lapply(tmp,function(x) x$filename))
@@ -70,6 +71,21 @@ load_edfs.itrackR <- function(obj,path='.',pattern='*.edf',samples=FALSE,recursi
 
 
   return(obj)
+
+}
+
+check_for_samples <- function(obj)
+{
+
+  if(length(obj$samples)<length(obj$edfs))
+  {
+    print('Sample data not loaded yet. Loading now (this make take a while)...')
+    obj <- load_samples(obj)
+  }
+
+
+
+ return(obj)
 
 }
 
@@ -94,8 +110,6 @@ plot <- function(obj,zoom=FALSE){
 }
 
 plot.itrackR <- function(obj,zoom=FALSE){
-
-
 
   plt <- ggplot2::ggplot(obj$fixations,ggplot2::aes(x=gavx,y=gavy)) + ggplot2::geom_point() + ggplot2::facet_wrap(~ID)
 
@@ -161,44 +175,54 @@ add_behdata <- function(obj,beh){
 }
 
 
-epoch_samples <- function(obj,timevar,field='paL',epoch=c(-100,100))
+epoch_samples <- function(obj,timevar,field='pa',epoch=c(-100,100),cleanup=F)
 {
 
-  allids <- unique(obj$header[obj$idvar])
+  obj <- check_for_samples(obj) #if we haven't loaded sample data yet, this will do it
 
-  allepochs <- list()
+  # allepochs <- list()
 
-  for(id in allids)
+  for(i in 1:length(obj$edfs))
   {
 
-#
-#   thisepoch <- list()
-#   thisepoch$name <- timevar
-#   thisepoch$epoch <- epoch
-#   thisepoch$field <- field
+#     justfile <- sub("^([^.]*).*", "\\1", basename(obj$edfs[[i]]))
+#     id <- as.numeric(gsub("([0-9]*).*","\\1",justfile))
 
-  subdata <- get_subdata(obj,id,fields=c('header','samples','messages'))
+    id <- edf2id(obj$edfs[[i]])
 
-  thisepoch <- edfR::epoch.samples(timevar,subdata$samples,sample.field=field,epoch=epoch,eyetrial=T,messages=subdata$messages)
+    header <- subset(obj$header,ID==id)
+    events <- header[[timevar]][!is.na(header[[timevar]])]
+    trials <- header$eyetrial[!is.na(header[[timevar]])]
+    samples <- readRDS(obj$samples[[i]])
 
-  allepochs <- c(allepochs,thisepoch)
-}
+    thisepoch <- edfR::epoch.samples(events,samples,sample.field=field,epoch=epoch,eyetrial=T)
 
-  obj$epochs <- allepochs
+    thisepoch$ID <- rep(id,length(trials))
+    thisepoch$event <- timevar
+    thisepoch$eyetrial <- trials
+    obj$epochs[[timevar]][[i]] <- thisepoch
+
+    rm(samples)
+    gc()
+
+  }
+
+  # obj$epochs[[timevar]] <- allepochs
+
 
   return(obj)
 
 }
 
 
-get_subdata <- function(obj,ID,fields = c('header','samples'))
+get_subdata <- function(obj,id,fields = c('header','samples'))
 {
   subobj <- itrackr()
 
   for(f in fields)
   {
 
-  subobj[[f]] <- obj[[f]][obj[[f]]$ID==ID,]
+    subobj[[f]] <- subset(obj[[f]],ID==id)
 
   }
 
@@ -210,20 +234,156 @@ get_subdata <- function(obj,ID,fields = c('header','samples'))
 }
 
 
-load_samples <- function(obj){
+load_samples <- function(obj,outdir=tempdir()){
 
   allsamps <- list()
 
   i <- 1
   for(edf in obj$edfs)
   {
-    alldata <- edfR::edf.batch(edf,samples=T,do.plot=F)
+    alldata <- edfR::edf.trials(edf,samples = T,eventmask = T)
 
-    allsamps[[i]] <- alldata$samples
+    samps <- alldata$samples
+    rm(alldata)
+
+    if(all(is.na(samps$paL))){
+      samps <- dplyr::select(samps,-paL,-gxL,gyL)
+      samps <- dplyr::rename(samps,
+                             pa = paR,
+                             gx = gxR,
+                             gy = gyR)
+      }
+    else{
+      samps <- dplyr::select(samps,-paR,-gxR,gyR)
+      samps <- dplyr::rename(samps,
+                           pa = paL,
+                           gx = gxL,
+                           gy = gyL)
+    }
+
+
+    id <- edf2id(edf)
+    fname <- file.path(outdir,paste0(id,'_samp.rds'))
+    saveRDS(samps,fname,compress = T)
+    allsamps[[i]] <- fname
+    rm(samps)
     i <- i+1
   }
 
 
-  return(allsamps)
+  obj$samples <- allsamps
+  return(obj)
 
 }
+
+remove_blinks <- function(obj)
+{
+
+  obj <- check_for_samples(obj)
+
+  for(i in 1:length(obj$edfs))
+  {
+
+    samps<- readRDS(obj$samples[[i]])
+
+    blinks <- as.logical(samps$blink)
+    samps$pa[blinks] <-NA
+    samps$gx[blinks] <-NA
+    samps$gy[blinks] <-NA
+
+    saveRDS(samps,obj$samples[[i]],compress = T)
+    rm(samps)
+
+  }
+  return(obj)
+}
+
+get_all_epochs <- function(obj,epochname,baseline=NULL,baseline.method='percent',shape='wide',beh=NULL)
+{
+
+  window_start <- obj$epochs[[epochname]][[1]]$epoch.window[1]
+  window_end <- obj$epochs[[epochname]][[1]]$epoch.window[2]
+
+  epochs <- obj$epochs[[epochname]]
+  epochs <- lapply(epochs,function(x) cbind(x$ID,x$eyetrial,x$epochs))
+  epochs <- do.call(rbind,epochs)
+
+  if(!is.null(baseline))
+  {
+    epochs_b <- baseline_epochs(epochs[,-c(1,2)],baseline=baseline,method=baseline.method)
+    epochs <- cbind(epochs[,1:2],epochs_b)
+  }
+
+  timenames <- paste0('t',seq(window_start,window_end-1))
+  timenames <- gsub('-','_',timenames)
+  colnames(epochs) <- c('ID','eyetrial',timenames)
+
+  epochs <- as.data.frame(epochs)
+
+  if(shape=='long')
+    epochs <- tidyr::gather_(epochs,'timepoint','value',timenames)
+
+  if(!is.null(beh))
+  {
+
+    if(is.logical(beh) && beh==TRUE)
+      behnames <- names(obj$beh)
+    else
+      behnames <- unique(c('ID','eyetrial',beh))
+
+    epochs <- dplyr::left_join(epochs,dplyr::select_(obj$beh,.dots=behnames),by=c('ID','eyetrial'))
+
+    if(shape=='long')
+      epochs <- dplyr::arrange(epochs,ID,eyetrial,timepoint)
+    else
+      epochs <- dplyr::arrange(epochs,ID,eyetrial)
+
+  }
+
+
+
+  return(epochs)
+}
+
+
+baseline_epochs <- function(epochs,baseline=c(1,100),method='percent'){
+
+
+  mn <- rowMeans(epochs[,baseline[1]:baseline[2]],na.rm=T)
+
+  epoch_b <- sweep(epochs,1,mn,FUN='-')
+  epoch_b <- sweep(epoch_b,1,mn,FUN='/')
+
+  return(epoch_b)
+}
+
+plot_random_epochs <- function(epochs,n=100)
+{
+
+  if('timepoint' %in% names(epochs))
+  {
+    epochs <- dplyr::select(epochs,ID,eyetrial,timepoint,value)
+    epochs <- tidyr::spread(epochs,timepoint,value)
+  }
+
+
+  epochs <- dplyr::select(epochs,matches('^t'))
+
+  tmp <- dplyr::sample_n(epochs,n)
+  matplot(t(as.matrix(tmp)),type='l',lty=1)
+
+}
+
+remove_ext <- function(filename){
+  justfile <- sub("^([^.]*).*", "\\1", basename(filename))
+  return(justfile)
+}
+
+edf2id <- function(edf){
+
+  justfile <- remove_ext(edf)
+  id <- as.numeric(gsub("([0-9]*).*","\\1",justfile))
+  return(id)
+}
+
+
