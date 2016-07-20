4eb123e43490f4500d0a181db87f45a872c12ed6
Jason Hubbard <jashubbard@gmail.com>
2016-07-20 15:54:28 -0700

    added tests

---
+++ tests/testthat/test_basic.R
@@ -0,0 +1,15 @@
+context('basics')
+
+z <- itrackr(path=itrackR.data('path'))
+
+test_that("basic tests", {
+
+  expect_is(z,'itrackR')
+  expect_equal(length(z$edfs),2)
+  expect_equal(length(z$subs),2)
+  expect_equal(z$samples,list())
+
+
+
+
+})
---
+++ tests/testthat/test_samples.R
@@ -0,0 +1,45 @@
+library(itrackR)
+
+context("samples")
+
+z <- itrackr(edfs=itrackR.data('edfs')[1])
+z <- load_samples(z,force=T, parallel=F)
+
+test_that("Checking sample loading (serial)",
+          {
+            expect_equal(length(z$samples),1)
+            expect_true(file.exists(z$samples[[1]]))
+
+          })
+
+
+test_that("sample blink removal", {
+
+  z <- remove_blinks(z, interpolate = T)
+  samp <- readRDS(z$samples[[1]])
+
+  expect_is(samp,'data.table')
+  expect_false(any(is.na(samp$pa)))
+  expect_false(any(samp$pa<500))
+
+
+})
+
+
+test_that("epoching samples", {
+
+  z <- find_messages(z,varnames = c('STIMONSET'),patterns = c('STIMONSET'),timestamp = T)
+
+  expect_true('STIMONSET' %in% names(z$header))
+
+  z <- epoch_samples(z,'STIMONSET',epoch = c(-500,500))
+
+  expect_match(names(z$epochs$samples)[[1]],'STIMONSET')
+  expect_equal(length(z$epochs$samples$STIMONSET),1)
+  expect_is(z$epochs$samples$STIMONSET[[1]]$epochs,'matrix')
+  expect_equal(dim(z$epochs$samples$STIMONSET[[1]]$epochs),c(1320,1000))
+  expect_gt(var(z$epochs$samples$STIMONSET[[1]]$epochs[100,],na.rm=T),0)
+  # allepochs <- get_all_epochs(z,'STIMONSET',shape='wide',baseline=NULL,beh=T)
+
+
+})
