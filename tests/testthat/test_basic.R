274fe7a308ce9042a0be3ca2c610a97c96527903
Jason Hubbard <jashubbard@gmail.com>
2016-07-22 09:20:46 -0700

    confirming tests are being run in Travis-CI

--- tests/testthat/test_basic.R
+++ tests/testthat/test_basic.R
@@ -5,7 +5,7 @@ z <- itrackr(path=itrackR.data('path'))
 test_that("basic tests", {
 
   expect_is(z,'itrackR')
-  expect_equal(length(z$edfs),2)
+  expect_equal(length(z$edfs),5)
   expect_equal(length(z$subs),2)
   expect_equal(z$samples,list())
 
