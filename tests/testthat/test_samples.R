efb5b3e7e3977d0ce82ae71730d11bd84582cae3
jashubbard <jashubbard@gmail.com>
2016-11-22 13:33:13 -0800

    adding output to test_samples

--- tests/testthat/test_samples.R
+++ tests/testthat/test_samples.R
@@ -1,9 +1,8 @@
 context('samples')
 
-z <- itrackr(path=itrackR.data('path'))
+z <- itrackr(edfs=itrackR.data('edfs')[1])
 
 test_that('Sample Functions', {
-
 
 
   z <- set_index(z,varnames=c('Block','Trial'), patterns=c('^BLOCK [0-9]*','^TRIAL [0-9]*'), numeric.only=T)
@@ -15,8 +14,12 @@ test_that('Sample Functions', {
   expect_equal(length(z$samples),length(z$subs))
   expect_true(file.exists(z$samples[[1]]))
 
+  print('Done loading samples')
 
   z <- remove_blinks(z, interpolate = T)
+
+  print('Done removing blinks')
+
   samp <- get_samples(z,z$subs[1])
 
   expect_is(samp,'data.frame')
@@ -26,8 +29,10 @@ test_that('Sample Functions', {
 
   expect_true('STIMONSET' %in% names(z$header))
 
+  print('Getting epochs...')
   epochs <- get_sample_epochs(z, factors = c('Task','Conflict'), parallel=T,
                               event='STIMONSET', epoch = c(-500,500), baseline= c(-100,0),aggregate=T)
+
 
   expect_is(epochs,'data.frame')
   expect_true(all(c('Task','Conflict') %in% names(epochs)))
